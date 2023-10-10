4032ff56cad3e40816004730eee0c34d767a0e67
Pleket <50240262+Pleket@users.noreply.github.com>
2023-10-10 15:43:00 +0200

    Network added, bugfix

--- JTT.py
+++ JTT.py
@@ -10,7 +10,7 @@ import os
 import tarfile
 from PIL import Image
 
-def train(resized_directory):
+def train_resnet50(resized_directory):
     num_classes = 200
     input_shape = (224, 224, 3)  # RGB images
 
@@ -35,6 +35,53 @@ def train(resized_directory):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
     model.to(device)
+
+    epochs = 1
+    steps = 0
+    running_loss = 0
+    print_every = 10
+    train_losses, test_losses = [], []
+    for epoch in range(epochs):
+        for inputs, labels in train_loader:
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in test_loader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        test_loss += batch_loss.item()
+                        
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                train_losses.append(running_loss/len(train_loader))
+                test_losses.append(test_loss/len(test_loader))                    
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                    f"Train loss: {running_loss/print_every:.3f}.. "
+                    f"Test loss: {test_loss/len(test_loader):.3f}.. "
+                    f"Test accuracy: {accuracy/len(test_loader):.3f}")
+                running_loss = 0
+                model.train()
+    torch.save(model, 'aerialmodel.pth')
+
+    # Plot the training and validation losses
+    plt.plot(train_losses, label='Training loss')
+    plt.plot(test_losses, label='Validation loss')
+    plt.legend(frameon=False)
+    plt.show()
 
 def preprocess():
     #resize to 224x224 pixels
@@ -66,7 +113,7 @@ def preprocess():
     
     batch_size = 32
 
-    full_data = torchvision.datasets.ImageFolder(root=resized_directory, transform=transforms)
+    full_data = torchvision.datasets.ImageFolder(root=target_directory, transform=transforms)
 
     # Split the dataset into training and validation sets
     train_size = int(0.8 * len(full_data))
