d7ffd613b9dfddc929a5cec4b52980cb2d7033d2
FleurEoK <f.a.ensink.op.kemna@student.tue.nl>
2023-10-20 09:57:25 +0200

    dro updates

--- losses..py
+++ losses..py
@@ -12,6 +12,35 @@ def cvar_value(p, v, reg):
     return torch.dot(p, v) - reg * kl
 
 
+def bisection(eta_min, eta_max, f, tol, maxiter):
+    minimum = f(eta_min)
+    maximum = f(eta_max)
+
+    while minimum > 0 or maximum<0:
+        length = eta_max - eta_min
+        if minimum>0:
+            eta_max = eta_min
+            eta_min = eta_min-2*length
+
+        if maximum<0:
+            eta_min = eta_max
+            eta_max = eta_max+2*length
+
+        minimum = f(eta_min)
+        maximum = f(eta_max)
+    
+    for _ in range(maxiter):
+        eta = (eta_min+eta_max)/2
+        value = f(eta)
+        if abs(value)<=tol:
+            return eta
+        if value>0:
+            eta_max = eta
+        elif value<0:
+            eta_min = eta
+    
+    return (eta_min+eta_max)/2
+
 class loss(torch.nn.Module):
     def __init__(self, alpha, tol, maxiter, reg):
         super(loss, self).__init__()
@@ -31,6 +60,18 @@ class loss(torch.nn.Module):
             def p(eta):
                 x = (v-eta)/reg
                 return torch.min(torch.exp(x), torch.Tensor([1.0/alpha]).type(x.dtype))/m
+            
+            def bisection_target(eta):
+                return 1.0 - p(eta).sum()
+            
+            eta_min = reg * torch.logsumexp(v / reg - np.log(m), 0)
+            eta_max = v.max()
 
-    def forward(self, p):
-        return cvar_value(p, self.v, self.reg)
+            if abs(bisection_target(eta_min))<=self.tol:
+                return p(eta_min)
+        else:
+            return 'regularizer is under zero'
+
+
+    def forward(self, v, p):
+        return cvar_value(p, v, self.reg)
